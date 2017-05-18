f198ec51c1c8cf4a429b522d4f422ab02424e97a
vikaschoudhary16 <choudharyvikas16@gmail.com>
2017-05-18 02:40:24 -0400

    more_fixes

--- kuryr_kubernetes/clients.py
+++ kuryr_kubernetes/clients.py
@@ -12,121 +12,34 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-import contextlib
-import itertools
-import os
 
-from oslo_log import log as logging
-from oslo_serialization import jsonutils
-import requests
+from kuryr.lib import utils
 
-from kuryr.lib._i18n import _
 from kuryr_kubernetes import config
-from kuryr_kubernetes import exceptions as exc
+from kuryr_kubernetes import k8s_client
 
-LOG = logging.getLogger(__name__)
+_clients = {}
+_NEUTRON_CLIENT = 'neutron-client'
+_KUBERNETES_CLIENT = 'kubernetes-client'
 
 
-class K8sClient(object):
-    # REVISIT(ivc): replace with python-k8sclient if it could be extended
-    # with 'WATCH' support
+def get_neutron_client():
+    return _clients[_NEUTRON_CLIENT]
 
-    def __init__(self, base_url):
-        self._base_url = base_url
-        cert_file = config.CONF.kubernetes.ssl_client_crt_file
-        key_file = config.CONF.kubernetes.ssl_client_key_file
-        ca_crt_file = config.CONF.kubernetes.ssl_ca_crt_file
-        self.verify_server = config.CONF.kubernetes.ssl_verify_server_crt
-        token_file = config.CONF.kubernetes.token_file
-        import pdb; pdb.set_trace()
-        if token_file:
-            with open(token_file, 'r') as f:
-            	self.token = f.readline().rstrip('\n')
-        else:
-            if cert_file and not os.path.exists(cert_file):
-                raise RuntimeError(
-                    _("Unable to find ssl cert_file  : %s") % cert_file)
-            if key_file and not os.path.exists(key_file):
-                raise RuntimeError(
-                    _("Unable to find ssl key_file : %s") % key_file)
-        if self.verify_server:
-            if not ca_crt_file:
-                raise RuntimeError(
-                    _("ssl_ca_crt_file cannot be None"))
-            elif not os.path.exists(ca_crt_file):
-                raise RuntimeError(
-                    _("Unable to find ca cert_file  : %s") % ca_crt_file)
-            else:
-                self.verify_server = ca_crt_file
 
-        self.cert = (cert_file, key_file)
+def get_kubernetes_client():
+    return _clients[_KUBERNETES_CLIENT]
 
-    def get(self, path):
-        LOG.debug("Get %(path)s", {'path': path})
-        url = self._base_url + path
-        response = requests.get(url, cert=self.cert,
-                                verify=self.verify_server,
-                                headers={'Authorization': 'Bearer %s' % self.token})
-        if not response.ok:
-            raise exc.K8sClientException(response.text)
-        return response.json()
 
-    def annotate(self, path, annotations, resource_version=None):
-        """Pushes a resource annotation to the K8s API resource
+def setup_clients():
+    setup_neutron_client()
+    setup_kubernetes_client()
 
-        The annotate operation is made with a PATCH HTTP request of kind:
-        application/merge-patch+json as described in:
 
-        https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#patch-operations  # noqa
-        """
-        LOG.debug("Annotate %(path)s: %(names)s", {
-            'path': path, 'names': list(annotations)})
-        url = self._base_url + path
-        while itertools.count(1):
-            data = jsonutils.dumps({
-                "metadata": {
-                    "annotations": annotations,
-                    "resourceVersion": resource_version,
-                }
-            }, sort_keys=True)
-            response = requests.patch(url, data=data, headers={
-                'Content-Type': 'application/merge-patch+json',
-                'Accept': 'application/json',
-                'Authorization': 'Bearer %s' % self.token
-            }, cert=self.cert, verify=self.verify_server)
-            if response.ok:
-                return response.json()['metadata']['annotations']
-            if response.status_code == requests.codes.conflict:
-                resource = self.get(path)
-                new_version = resource['metadata']['resourceVersion']
-                retrieved_annotations = resource['metadata'].get(
-                    'annotations', {})
+def setup_neutron_client():
+    _clients[_NEUTRON_CLIENT] = utils.get_neutron_client()
 
-                for k, v in annotations.items():
-                    if v != retrieved_annotations.get(k, v):
-                        break
-                else:
-                    # No conflicting annotations found. Retry patching
-                    resource_version = new_version
-                    continue
-                LOG.debug("Annotations for %(path)s already present: "
-                          "%(names)s", {'path': path,
-                                        'names': retrieved_annotations})
-            raise exc.K8sClientException(response.text)
 
-    def watch(self, path):
-        params = {'watch': 'true'}
-        url = self._base_url + path
-
-        # TODO(ivc): handle connection errors and retry on failure
-        while True:
-            with contextlib.closing(
-                    requests.get(url, params=params, stream=True,
-                    cert=self.cert, verify=self.verify_server,
-                    headers={'Authorization': 'Bearer %s' % self.token})) as response:
-                if not response.ok:
-                    raise exc.K8sClientException(response.text)
-                for line in response.iter_lines(delimiter='\n'):
-                    line = line.strip()
-                    if line:
-                        yield jsonutils.loads(line)
+def setup_kubernetes_client():
+    _clients[_KUBERNETES_CLIENT] = k8s_client.K8sClient(
+        config.CONF.kubernetes.api_root)
--- kuryr_kubernetes/k8s_client.py
+++ kuryr_kubernetes/k8s_client.py
@@ -40,7 +40,7 @@ class K8sClient(object):
         token_file = config.CONF.kubernetes.token_file
         if token_file:
             with open(token_file, 'r') as f:
-            	self.token = f.readline()
+            	self.token = f.readline().rstrip('\n')
         else:
             if cert_file and not os.path.exists(cert_file):
                 raise RuntimeError(
@@ -65,7 +65,7 @@ class K8sClient(object):
         url = self._base_url + path
         response = requests.get(url, cert=self.cert,
                                 verify=self.verify_server,
-                                headers={'Authorization': 'Bearer %s' % token})
+                                headers={'Authorization': 'Bearer %s' % self.token})
         if not response.ok:
             raise exc.K8sClientException(response.text)
         return response.json()
@@ -91,8 +91,8 @@ class K8sClient(object):
             response = requests.patch(url, data=data, headers={
                 'Content-Type': 'application/merge-patch+json',
                 'Accept': 'application/json',
-            }, cert=self.cert, verify=self.verify_server,
-            headers={'Authorization': 'Bearer %s' % self.token})
+                'Authorization': 'Bearer %s' % self.token
+            }, cert=self.cert, verify=self.verify_server)
             if response.ok:
                 return response.json()['metadata']['annotations']
             if response.status_code == requests.codes.conflict:
@@ -122,7 +122,7 @@ class K8sClient(object):
             with contextlib.closing(
                     requests.get(url, params=params, stream=True,
                     cert=self.cert, verify=self.verify_server,
-                    headers={'Authorization': 'Bearer %s' % token})) as response:
+                    headers={'Authorization': 'Bearer %s' % self.token})) as response:
                 if not response.ok:
                     raise exc.K8sClientException(response.text)
                 for line in response.iter_lines(delimiter='\n'):
